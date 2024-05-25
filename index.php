1b4459bfd3cb001ef54c48f9aa1fa9b9e3865533
rashiknahian <rashik@nahian.me>
2024-05-25 11:21:37 +0600

    Backend Done

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <h1> <?php echo "Hello World";?></h1>
+   <h2> <?php echo "Hello Bangladesh";?></h2>
+   <h3> <?php echo "Hello Dhaka";?></h3>
+</body>
+</html>
